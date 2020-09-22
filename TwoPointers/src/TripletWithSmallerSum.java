e7269c21caafb3f3be0c7ba4a53e2175b72b4071
Atul <atulflexdeveloper@gmail.com>
2020-09-23 03:01:07 +0530

    Triplet with Smaller Sum

---
+++ TwoPointers/src/TripletWithSmallerSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class TripletWithSmallerSum {
+    public static int searchTriplets(int[] arr, int target) {
+        int count = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            int cnt = getTripletsCount(arr, i+1, target - arr[i]);
+            count += cnt;
+        }
+
+        return count;
+    }
+
+    private static int getTripletsCount(int[] arr, int left, int target) {
+        int right = arr.length - 1;
+        int count = 0;
+        while (left < right) {
+            if (arr[left] + arr[right] < target) {
+                count += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 0, 2, 3 }, 3));
+        System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 4, 2, 1, 3 }, 5));
+    }
+}
