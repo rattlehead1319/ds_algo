53879f991f9d18c18b6d25bbde2370a12b4db815
rattlehead1319 <atulflexdeveloper@gmail.com>
2020-10-20 10:12:08 +0530

    Cyclic Sort

---
+++ CyclicSort/src/CyclicSort.java
@@ -0,0 +1,39 @@
+class CyclicSort {
+
+    public static void sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int j = nums[i] - 1;
+            if (nums[i] != nums[j])
+                swap(nums, i, j);
+            else
+                i++;
+        }
+    }
+
+    private static void swap(int[] nums, int x, int y) {
+        int temp = nums[y];
+        nums[y] = nums[x];
+        nums[x] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 1, 5, 4, 2 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 2, 6, 4, 3, 1, 5 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 1, 5, 6, 4, 3, 2 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+}
