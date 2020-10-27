ccd234e11fd0bbd27bd73a63cc21ee6da16fa766
rattlehead1319 <atulflexdeveloper@gmail.com>
2020-10-27 10:37:46 +0530

    Added Level Average

---
+++ TreeBreadthFirstSearch/src/LevelAverage.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class LevelAverage {
+    public static List<Double> findLevelAverages(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            Double sum = 0.0;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                sum += currentNode.val;
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(sum/levelSize);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<Double> result = LevelAverage.findLevelAverages(root);
+        System.out.print("Level averages are: " + result);
+    }
+}
