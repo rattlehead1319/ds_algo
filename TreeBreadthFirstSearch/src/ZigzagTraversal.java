b5434a882ca163222db30cc802d883d6ff03755c
rattlehead1319 <atulflexdeveloper@gmail.com>
2020-10-27 07:55:08 +0530

    Added Zig Zag Traversal

---
+++ TreeBreadthFirstSearch/src/ZigzagTraversal.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class ZigzagTraversal {
+    public static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flipOrder = false;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if (!flipOrder) {
+                    currentLevel.add(currentNode.val);
+                } else {
+                    currentLevel.add(0, currentNode.val);
+                }
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            flipOrder = !flipOrder;
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        root.right.left.left = new TreeNode(20);
+        root.right.left.right = new TreeNode(17);
+        List<List<Integer>> result = ZigzagTraversal.traverse(root);
+        System.out.println("Zigzag traversal: " + result);
+    }
+}
