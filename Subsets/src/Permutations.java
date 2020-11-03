7a807700e55f9e8c19f4b2e141651c2afbf1619a
Atul <atulflexdeveloper@gmail.com>
2020-11-04 01:47:00 +0530

    Permutations

---
+++ Subsets/src/Permutations.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Permutations {
+
+    public static List<List<Integer>> findPermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+        permutations.add(new ArrayList<>());
+        for (Integer num : nums) {
+            int n = permutations.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> oldPermutation = permutations.poll();
+                for (int j = 0; j < oldPermutation.size() + 1; j++) {
+                    List<Integer> set = new ArrayList<>(oldPermutation);
+                    set.add(j, num);
+                    if (set.size() == nums.length) {
+                        result.add(set);
+                    } else {
+                        permutations.add(set);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = Permutations.findPermutations(new int[] { 1, 3, 5 });
+        System.out.print("Here are all the permutations: " + result);
+    }
+}
