d0f6234c949a85de4b5446166e311e11bd4d554d
rattlehead1319 <51386043+rattlehead1319@users.noreply.github.com>
2021-05-16 00:49:37 +0530

    KthMissingPositiveNumber

---
+++ LeetCode/src/challenge/KthMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+public class KthMissingPositiveNumber {
+  public int findKthPositive(int[] arr, int k) {
+        int i = 1;
+        int j = 0;
+        while (i <= arr[arr.length - 1] || j < arr.length) {
+            if (k == 0) {
+                return i-1;
+            }
+            if (i == arr[j]) {
+                j++; 
+            } else {
+                k--;
+            }
+            i++;
+        }
+        while (k-- > 0) {
+            i++;
+        }
+        return i-1;
+    }
+}
