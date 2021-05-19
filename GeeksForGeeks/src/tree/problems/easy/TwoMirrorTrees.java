8a398c3502d54967299eba6bcfcc7f457e9bfaed
Atul <atulflexdeveloper@gmail.com>
2021-05-20 05:13:03 +0530

    TwoMirror

---
+++ GeeksForGeeks/src/tree/problems/easy/TwoMirrorTrees.java
@@ -0,0 +1,16 @@
+package tree.problems.easy;
+
+public class TwoMirrorTrees {
+
+    boolean areMirror(Node a, Node b) {
+        if (a == null && b == null) {
+            return true;
+        }
+
+        if (a == null || b == null) {
+            return false;
+        }
+
+        return a.data == b.data && areMirror(a.left, b.right) && areMirror(a.right, b.left);
+    }
+}
